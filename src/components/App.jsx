7c2afa53b506204b0ae5552ce1bec5171ca1dc55
Ketomlinson <47015963+Ketomlinson@users.noreply.github.com>
2021-01-20 17:49:47 -0800

    Day 1 Questions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,4 +6,16 @@ function App() {
   return <div className="app">Trivia!</div>;
 }
 
+var questionNumber = 0;
+
+function Question({text}) {
+  return (
+  <div className="question">
+    <Question text = {text}/>
+    <Question text = "test"/>
+  </div>
+  );
+}
+
+
 export default App;
