83a548b5cb95cacb5350c96a38bd1d4a17c23c87
Jordan Sosa <44591365+J0rdanS23@users.noreply.github.com>
2021-02-03 17:57:34 -0800

    day1 and half of day2 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,46 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  return (
+
+  <div className="app">Trivia!
+    <Question text = {data[questionNumber].question.text}/>
+    <NextQuestion/>
+  </div>
+  );
 }
 
 var questionNumber = 0;
 
-function Question({text}) {
+function Question({text, answers}) {
+  //var numOfChoices = data[questionNumber].question.choice.length();
+  /*var answers = "";
+  for(var i = 0; i < numOfChoices; i++){
+    answers += "<Answer text=\"
+  }
+  */
+
+  //answers.map();
+
   return (
-  <div className="question">
-    <Question text = {text}/>
-    <Question text = "test"/>
+  <div className="Question">{text}
+    <Answer text={data[questionNumber].question.choices[0]}/>
+    <Answer text={data[questionNumber].question.choices[1]}/>
+    <Answer text={data[questionNumber].question.choices[2]}/>
+    <Answer text={data[questionNumber].question.choices[3]}/>
   </div>
+  );
+}
+
+function NextQuestion(){
+  return(
+    <div className="NextQuestion"><button>Next Question</button></div>
+  );
+}
+
+function Answer({text}){
+  return(
+    <div className="Answer">{text}</div>
   );
 }
 
